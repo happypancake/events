67006b784f9880ddc71c77702717b521321e117a
Rinat Abdullin <rinat.abdullin@gmail.com>
2014-04-24 10:21:52 +0600

    More helper methods in UUID, fixed sequence bug

--- uuid.go
+++ uuid.go
@@ -10,19 +10,45 @@ import (
 
 var (
 	seq  uint32
-	node = uuid.NodeID()
+	node = getNodeUint32()
 )
 
-func NewSequentialUUID() []byte {
+func getNodeUint32() uint32 {
+	n := uuid.NodeID()
+	return binary.BigEndian.Uint32(n)
+}
+
+type UUID []byte
+
+// 8 bytes of UNIXNANO
+// 4 bytes of counter
+// 4 bytes of hardware address
+//type UUID []byte
+
+func NewSequentialUUID() UUID {
 	uuid := make([]byte, 16)
 
 	nano := time.Now().UnixNano()
+	incr := atomic.AddUint32(&seq, 1)
+
 	binary.BigEndian.PutUint64(uuid[0:], uint64(nano))
-
-	copy(uuid[10:], node)
-	atomic.AddUint32(&seq, 1)
-
-	binary.BigEndian.PutUint32(uuid[8:], seq)
+	binary.BigEndian.PutUint32(uuid[8:], incr)
+	binary.BigEndian.PutUint32(uuid[12:], node)
 
 	return uuid
+}
+
+func (u UUID) Bytes() []byte {
+	return []byte(u)
+}
+
+func (u UUID) Time() time.Time {
+	nsec := binary.BigEndian.Uint64([]byte(u))
+	return time.Unix(0, int64(nsec))
+}
+func (u UUID) Node() uint32 {
+	return binary.BigEndian.Uint32([]byte(u)[12:])
+}
+func (u UUID) Sequence() uint32 {
+	return binary.BigEndian.Uint32([]byte(u)[8:])
 }
